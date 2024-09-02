2d1fa1a79ca29ff8096c5cccc3431edf4a34d8b1
ericx5 <33452836+ericx5@users.noreply.github.com>
2024-09-02 15:31:00 +0200

    Initial version
    Simplest piping for blocks in .csv

---
+++ GunnerusConnection.js
@@ -0,0 +1,13 @@
+var Connection_Data = {
+    "connections":[
+        {
+            "start": "Engine Room",
+            "end": "Fuel Oil Tank 2",
+            "diameter": 400
+          },
+          
+    ]
+}
+
+
+export default Connection_Data;
