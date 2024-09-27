01442681f704d390a7f8fe4ce6eaf22debcc4d23
WIN-NTNU-NO\jisangh <jisang.ha@ntnu.no>
2024-09-27 14:08:59 +0200

    no message

--- GunnerusConnection.js
+++ GunnerusConnection.js
@@ -6,6 +6,78 @@ var Connection_Data = {
             "diameter": 400
           },
           
+
+    {
+        "start": "Hull 1",
+        "end": "Engine Room",
+        "diameter": 400
+    },
+    {
+        "start": "Engine Room",
+        "end": "Cargo Hold",
+        "diameter": 300
+    },
+    {
+        "start": "Cargo Hold",
+        "end": "Control Room",
+        "diameter": 200
+    },
+    {
+        "start": "Control Room",
+        "end": "Conference Room",
+        "diameter": 250
+    },
+    {
+        "start": "Conference Room",
+        "end": "Cabins",
+        "diameter": 300
+    },
+    {
+        "start": "Cabins",
+        "end": "Fuel Oil Tank 1",
+        "diameter": 350
+    },
+    {
+        "start": "Fuel Oil Tank 1",
+        "end": "Optional new DO tank",
+        "diameter": 400
+    },
+    {
+        "start": "Optional new DO tank",
+        "end": "Fresh Water",
+        "diameter": 150
+    },
+    {
+        "start": "Fresh Water",
+        "end": "Dry Tank",
+        "diameter": 100
+    },
+    {
+        "start": "Dry Tank",
+        "end": "Fuel Oil Tank 2",
+        "diameter": 120
+    },
+    {
+        "start": "Fuel Oil Tank 2",
+        "end": "Water Ballast",
+        "diameter": 140
+    },
+    {
+        "start": "Water Ballast",
+        "end": "Accomodation 1",
+        "diameter": 160
+    },
+    {
+        "start": "Accomodation 1",
+        "end": "Accomodation 2",
+        "diameter": 180
+    },
+    {
+        "start": "Accomodation 2",
+        "end": "Ventilation Electronics",
+        "diameter": 200
+    }
+          
     ]
 }
 
